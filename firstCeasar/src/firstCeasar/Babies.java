59578efb2ef29552fd6e64a1bb79fb56195dbc8a
mariasilva <marcia-nasare@hotmail.com>
2015-11-27 16:13:06 -0200

    babies continue

--- firstCeasar/src/firstCeasar/Babies.java
+++ firstCeasar/src/firstCeasar/Babies.java
@@ -43,19 +43,44 @@ public class Babies {
 	
 	//File is ordered 
 	int getRank(int year, String name, String gender){
-		int rank = 0;
+		int rank = -1;
+		int tempRank = 0;
 		String fname = "data/yob" + year + "txt";
 		FileResource fr = new FileResource(fname);
 		CSVParser parser = fr.getCSVParser(false);
 		for(CSVRecord rec : parser){
 			if(rec.get(1).equals(gender)){
+				tempRank ++;
 				if(rec.get(0).equals(name)){
+					System.out.println("line " + parser.getCurrentLineNumber());
+					System.out.println("tempRank " + tempRank);
 					rank = (int) parser.getCurrentLineNumber();
 					break;
 				}
 			}
 		}
-		
 		return rank;
 	}
+	
+	String getName(int year, int rank, String gender){
+		String name = "No name";
+		int tempRank = 0;
+		String fname = "data/yob" + year + "txt";
+		FileResource fr = new FileResource(fname);
+		CSVParser parser = fr.getCSVParser(false);
+		for(CSVRecord rec : parser){
+			if(rec.get(1).equals(gender)){
+				tempRank++;
+				if(tempRank == rank){
+					return rec.get(0);
+				}
+			}
+		}
+		return name;
+	}
+	
+	void whatIsNameInYear(String name, String gender){
+		
+	}
+	
 }
